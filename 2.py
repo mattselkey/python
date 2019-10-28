08679da1a01bc267cb83f4a53b0b4ad538988283
mattselkey <matt@mattselkey.com>
2019-10-28 22:09:05 +0100

    messing around with Python

--- 2.py
+++ 2.py
@@ -10,9 +10,17 @@ print(f)
 
 
 # # re-declaring the variable works
-print("this is a string" + 123)
+print("this is a string" + str(123))
 
 # # ERROR: variables of different types cannot be combined
-
+# # print("this is a string" + 123)
 
 # Global vs. local variables in functions
+p=0
+def someFunction():
+    global p
+    p="def"
+    print(p)
+
+someFunction()
+print(p)
