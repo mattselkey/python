00565ac8936277b6485af60ec91a7ed270e1953c
mattselkey <matt@mattselkey.com>
2019-10-31 20:30:29 +0100

    messing around with Python

--- 3.py
+++ 3.py
@@ -7,10 +7,12 @@ def func1():
     print("I am a function")
 
 # function that takes arguments
-
+def func2(arg1, arg2):
+    print(arg1,"",arg2)
 
 # function that returns a value
-
+def cube(x):
+    return x*x*x
 
 # function with default value for an argument
 
@@ -21,3 +23,6 @@ def func1():
 func1()
 print (func1())
 print (func1)
+func2(10,20)
+print(func2(10,20))
+print(cube(3))
