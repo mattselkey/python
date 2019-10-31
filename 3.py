9c80293e0e31bce7948819c9c2004eabbaa6b964
mattselkey <matt@mattselkey.com>
2019-10-31 20:38:23 +0100

    messing around with Python

--- 3.py
+++ 3.py
@@ -22,6 +22,12 @@ def power(num, x=1):
     return result
 
 #function with variable number of arguments
+def multi_add(*args):
+    result = 0
+    for x in args:
+        result = result + x
+    return result
+
 
 
 func1()
@@ -32,3 +38,5 @@ print(func2(10,20))
 print(cube(3))
 print (power(2))
 print (power(2,3))
+print (power(x=3, num=2))
+print (multi_add(4,5,10,4)), 
