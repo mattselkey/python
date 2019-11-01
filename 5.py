ad520d643fa1d100435fd6ee04679b364b544133
mattselkey <matt@mattselkey.com>
2019-11-01 21:59:15 +0100

    messing around with Python

--- 5.py
+++ 5.py
@@ -5,13 +5,14 @@
 def main():
   x = 0
 
-  # define a while loop
-  while (x<5):
-    print(x)
-    x = x+1
+    ## define a while loop
+    #while (x<5):
+    #  print(x)
+    #  x = x+1
 
   # define a for loop
-
+for x in range (5,10):
+  print(x)
 
   # use a for loop over a collection
     
