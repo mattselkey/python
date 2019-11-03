c22f31139f560d4e4b00d75dbf0828b0dc062674
mattselkey <matt@mattselkey.com>
2019-11-03 20:32:36 +0100

    messing around with python

--- 7.py
+++ 7.py
@@ -12,22 +12,22 @@ def main():
   today = date.today()
   print ("Today's date is ", today)
   
-  # print out the date's individual components
+# # print out the date's individual components
   print ("Date Components: ", today.day, today.month, today.year)
-  
-  # retrieve today's weekday (0=Monday, 6=Sunday)
+
+ # # retrieve today's weekday (0=Monday, 6=Sunday)
   print ("Today's Weekday #: ", today.weekday())
   days = ["monday","tuesday","wednesday","thursday","friday","saturday","sunday"]
   print ("Which is a " + days[today.weekday()])
   
-  ## DATETIME OBJECTS
-  # Get today's date from the datetime class
-  today = datetime.now()
-  print  ("The current date and time is ", today)
-  
-  # Get the current time
-  t = datetime.time(datetime.now())
-  print ("The current time is ", t)
+# ## DATETIME OBJECTS
+# # Get today's date from the datetime class
+# today = datetime.now()
+# print  ("The current date and time is ", today)
+# 
+# # Get the current time
+# t = datetime.time(datetime.now())
+# print ("The current time is ", t)
   
   
 if __name__ == "__main__":
