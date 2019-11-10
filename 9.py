7212d49060021f748d8903eb158f0081c0315d1e
mattselkey <matt@mattselkey.com>
2019-11-10 20:31:26 +0100

    more fun with python

--- 9.py
+++ 9.py
@@ -5,22 +5,27 @@
 from datetime import date
 from datetime import time
 from datetime import datetime
+from datetime import timedelta
 
 
 # construct a basic timedelta and print it
-
+print(timedelta(days=365, hours=5, minutes=1))
 
 # print today's date
-
+now = datetime.now()
+print("today is: " +str(now))
 
 # print today's date one year from now
-
+print("one year from now it will be: " + str(now + timedelta(days=365)))
 
 # create a timedelta that uses more than one argument
-
+print("In 2 days and 3 weeks, it will be " +
+    str(now + timedelta(days=2, weeks=3)))
 
 # calculate the date 1 week ago, formatted as a string
-
+t = datetime.now() - timedelta(weeks=1)
+s = t.strftime("%A %B %d, %Y")
+print ("One week ago it was: " + s)
 
 ### How many days until April Fools' Day?
 
