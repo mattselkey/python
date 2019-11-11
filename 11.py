b3b9c67a8dfffe155096e9489771f9c3b6a00e5f
mattselkey <matt@mattselkey.com>
2019-11-11 20:14:48 +0100

    file write

--- 11.py
+++ 11.py
@@ -4,10 +4,10 @@
 
 def main():  
   # Open a file for writing and create it if it doesn't exist
-  f = open("textfile.txt", "w+")
+  #f = open("textfile.txt", "w+")
 
   # Open the file for appending text to the end
-
+  f = open("textfile.txt", "a")
 
   # write some lines of data to the file
   for i in range(10):
