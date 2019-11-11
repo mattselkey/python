ad5755808602f7bfb47d02119ca75f48c03dd0bd
mattselkey <matt@mattselkey.com>
2019-11-11 20:39:03 +0100

    shell stuff

---
+++ 13.py
@@ -0,0 +1,33 @@
+#
+# Example file for working with filesystem shell methods
+#
+import os
+from os import path
+import shutil
+
+
+def main():
+  # make a duplicate of an existing file
+  if path.exists("textfile.txt"):
+    # get the path to the file in the current directory
+    src = path.realpath("textfile.txt")
+    
+    # let's make a backup copy by appending "bak" to the name
+    dst = src + ".bak"
+
+    # copy over the permissions, modification times, and other info
+    shutil.copy(src, dst)
+    shutil.copystat(src, dst)
+    
+
+    # rename the original file
+
+    
+    # now put things into a ZIP archive
+
+
+    # more fine-grained control over ZIP files
+
+      
+if __name__ == "__main__":
+  main()
