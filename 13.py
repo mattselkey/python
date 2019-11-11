75909fc35123056c7abd6fcc8d14556439d5f6d5
mattselkey <matt@mattselkey.com>
2019-11-11 20:44:41 +0100

    shell stuff

--- 13.py
+++ 13.py
@@ -5,6 +5,7 @@ import os
 from os import path
 import shutil
 from shutil import make_archive
+from zipfile import ZipFile
 
 def main():
   # make a duplicate of an existing file
@@ -18,21 +19,20 @@ def main():
     # copy over the permissions, modification times, and other info
     #shutil.copy(src, dst)
     #shutil.copystat(src, dst)
-    #os.rename("textfile.txt", "newfile.txt")
-
-
-    # rename the original file
-
-
 
     
+    # rename the original file
+    #os.rename("textfile.txt", "newfile.txt")
+        
     # now put things into a ZIP archive
     root_dir, tail = path.split(src)
     shutil.make_archive("archive", "zip", root_dir)
 
-
-
     # more fine-grained control over ZIP files
+    with ZipFile("testzip.zip","w") as newzip:
+      newzip.write("newfile.txt")
+      newzip.write("textfile.txt.bak")
+
 
       
 if __name__ == "__main__":
