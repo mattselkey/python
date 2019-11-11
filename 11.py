13fa5b0944f069fa0974c1e095dc33ad6a0b050e
mattselkey <matt@mattselkey.com>
2019-11-11 20:20:42 +0100

    file write

--- 11.py
+++ 11.py
@@ -7,16 +7,23 @@ def main():
   #f = open("textfile.txt", "w+")
 
   # Open the file for appending text to the end
-  f = open("textfile.txt", "a")
-
+  #f = open("textfile.txt", "a")
+  f = open("textfile.txt", "r")
+  
   # write some lines of data to the file
-  for i in range(10):
-    f.write("This is line " + str(i) + "\r\n")
+  #for i in range(10):
+  #  f.write("This is line " + str(i) + "\r\n")
   
   # close the file when done
 
   
-  # Open the file back up and read the contents
+## Open the file back up and read the contents
+  if f.mode == 'r':
+#  contents = f.read()
+#  print(contents)
+    fl = f.readlines()
+    for x in fl:
+      print(x)
 
     
 if __name__ == "__main__":
